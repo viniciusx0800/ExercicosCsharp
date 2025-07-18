a36c0a9f654b3c8ac20ddab3760eb0bde7a2c43c
Viniciusx0800 <vinioliveira2008y@gmail.com>
2025-07-18 14:08:55 -0300

    feat:Adicionar arquivos iniciais do projeto, incluindo csproj, modelo de Produto e arquivo de programa

---
+++ Atividade01/Models/Produto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Atividade01.Models
+{
+    public class Produto
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public decimal preco { get; set; }
+    }
+}
